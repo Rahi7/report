51f5ae4d324c0467656b18fda02dc03a91005b02
Rahi7 <100rabh105@gmail.com>
2025-06-07 19:29:43 +0530

    Removed link from vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://d828-2409-40e0-102f-8e4e-f1e9-6319-44ea-ec70.ngrok-free.app/', // Your backend URL
+      '/api': 'localhost:800', // Your backend URL
     },
   },
 });
